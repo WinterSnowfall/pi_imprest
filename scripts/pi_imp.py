692ecc1861eb3812434d7016cf59ffb40695bb1c
Winter Snowfall <WinterSnowfall@users.noreply.github.com>
2023-10-22 13:35:56 +0300

    Handle tasks in the pre-work wait interval + some minor formatting

--- scripts/pi_imp.py
+++ scripts/pi_imp.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''
 @author: Winter Snowfall
-@version: 2.42
-@date: 26/06/2023
+@version: 2.50
+@date: 20/10/2023
 '''
 
 import paramiko
@@ -10,15 +10,15 @@ import requests
 
 class imp:
     '''An imp - a mythical creature that sometimes does things, but mostly rests'''
-    
+
     HEADERS = {'content-type': 'application/json'}
     # default values, should be overwritten when creating an imp
     rest_endpoint = None
     rest_timeout = 10
     ssh_private_key = None
     ssh_timeout = 10
-    
-    def __init__(self, header, name, active, ip, username, password, command, expected, 
+
+    def __init__(self, header, name, active, ip, username, password, command, expected,
                  payload_true, payload_false, pre_task, pre_task_payload, pre_task_duration):
         self.header = header
         self.name = name
@@ -36,7 +36,7 @@ class imp:
         self.pre_task_payload = pre_task_payload
         self.pre_task_duration = pre_task_duration
         self.state = None
-    
+
     def stretch(self):
         if self.pre_task:
             if self.rest_endpoint is not None:
@@ -49,14 +49,14 @@ class imp:
                     raise Exception('The imp can\'t stretch without a payload!')
             else:
                 raise Exception('The imp can\'t stretch without an endpoint!')
-    
+
     def work(self):
         self.output = None
         self.errors = None
-        
+
         with paramiko.SSHClient() as ssh:
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            
+
             try:
                 if self.ssh_private_key is not None:
                     ssh.connect(self.ip, username=self.username, pkey=self.ssh_private_key, timeout=self.ssh_timeout)
@@ -64,20 +64,20 @@ class imp:
                     ssh.connect(self.ip, username=self.username, password=self.password, timeout=self.ssh_timeout)
                 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(self.command)
                 ssh_stdin.close()
-                
+
                 output = ssh_stdout.read().decode('utf-8').strip()
                 self.output = output if output is not None and output != '' else None
                 errors = ssh_stderr.read().decode('utf-8').strip()
                 self.errors = errors if errors is not None and errors != '' else None
             except:
                 raise Exception('The imp has failed its task!')
-    
+
     def report(self):
         if self.output is not None and self.output == self.expected:
             self.state = True
         else:
             self.state = False
-    
+
     def idle(self):
         if self.rest_endpoint is not None:
             try:
@@ -86,14 +86,14 @@ class imp:
                 raise Exception('The imp couldn\'t reach the REST endpoint!')
         else:
             raise Exception('The imp can\'t idle without an endpoint!')
-    
+
     def rest(self):
         if self.rest_endpoint is not None:
             try:
                 if self.state:
                     requests.post(self.rest_endpoint, json=self.payload_true, headers=self.HEADERS, timeout=self.rest_timeout)
                 else:
-                    requests.post(self.rest_endpoint, json=self.payload_false, headers=self.HEADERS, timeout=self.rest_timeout)  
+                    requests.post(self.rest_endpoint, json=self.payload_false, headers=self.HEADERS, timeout=self.rest_timeout)
             except:
                 raise Exception('The imp couldn\'t reach the REST endpoint!')
         else:
--- scripts/pi_imprest.py
+++ scripts/pi_imprest.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 '''
 @author: Winter Snowfall
-@version: 2.42
-@date: 26/06/2023
+@version: 2.50
+@date: 20/10/2023
 '''
 
 import paramiko
@@ -11,7 +11,7 @@ import json
 import logging
 from configparser import ConfigParser
 from os import path
-from time import sleep
+from time import time, sleep
 from pi_password import password_helper
 from pi_imp import imp
 # uncomment for debugging purposes only
@@ -34,12 +34,12 @@ logger.addHandler(logger_file_handler)
 
 def sigterm_handler(signum, frame):
     logger.info('The imps scatter due to the dragon SIGTERM...')
-    
+
     raise SystemExit(0)
 
 def sigint_handler(signum, frame):
     logger.debug('The imps scatter due to the death knight SIGINT...')
-    
+
     raise SystemExit(0)
 
 if __name__ == "__main__":
@@ -47,16 +47,16 @@ if __name__ == "__main__":
     signal.signal(signal.SIGTERM, sigterm_handler)
     # catch SIGINT and exit gracefully
     signal.signal(signal.SIGINT, sigint_handler)
-    
+
     # disable interpolation to allow the use of unescaped '%' in command strings
     configParser = ConfigParser(interpolation=None)
-    
+
     try:
         configParser.read(CONF_FILE_PATH)
-        
+
         general_section = configParser['GENERAL']
         LOGGING_LEVEL = general_section.get('logging_level').upper()
-        
+
         # remains set to 'INFO' if none of the other valid log levels are specified
         if LOGGING_LEVEL == 'DEBUG':
             logger.setLevel(logging.DEBUG)
@@ -66,7 +66,7 @@ if __name__ == "__main__":
             logger.setLevel(logging.ERROR)
         elif LOGGING_LEVEL == 'CRITICAL':
             logger.setLevel(logging.CRITICAL)
-        
+
         # note that the cron job mode is meant to be used primarily with ssh key authentication
         CRON_JOB_MODE = general_section.getboolean('cron_job_mode')
         REST_ENDPOINT = general_section.get('rest_endpoint')
@@ -77,11 +77,11 @@ if __name__ == "__main__":
         if SSH_KEY_AUTHENTICATION:
             SSH_PRIVATE_KEY_PATH = path.expanduser(general_section.get('ssh_private_key_path'))
         SSH_TIMEOUT = general_section.getint('ssh_timeout')
-    
+
     except:
         logger.critical('Could not parse configuration file. Please make sure the appropriate structure is in place!')
         raise SystemExit(1)
-    
+
     if SSH_KEY_AUTHENTICATION:
         try:
             SSH_PRIVATE_KEY = paramiko.RSAKey.from_private_key_file(SSH_PRIVATE_KEY_PATH)
@@ -93,23 +93,23 @@ if __name__ == "__main__":
     else:
         # read the master password from the command line
         password = input('Please enter the master password: ')
-        
+
         if password == '':
             logger.critical('No password has been provided - exiting.')
             raise SystemExit(3)
-        
+
         psw_helper = password_helper()
-        
+
     logger.info('The imps are being summoned...')
-    
+
     imp.rest_endpoint = REST_ENDPOINT
     imp.rest_timeout = REST_TIMEOUT
     imp.ssh_private_key = SSH_PRIVATE_KEY
     imp.ssh_timeout = SSH_TIMEOUT
-    
+
     current_task_no = 1
     imp_tasks = []
-    
+
     try:
         while True:
             # designation of the imp task
@@ -142,65 +142,78 @@ if __name__ == "__main__":
             current_task_pre_task_payload = json.loads(current_task_section.get('pre_task_payload'))
             # time in seconds for the pre-task to run
             current_task_pre_task_duration = current_task_section.getfloat('pre_task_duration')
-            
-            imp_tasks.append(imp(current_task_header, current_task_name, current_task_active, current_task_ip, 
-                                 current_task_username, current_task_password, current_task_command, current_task_expected, 
-                                 current_task_payload_true, current_task_payload_false, current_task_pre_task, 
+
+            imp_tasks.append(imp(current_task_header, current_task_name, current_task_active, current_task_ip,
+                                 current_task_username, current_task_password, current_task_command, current_task_expected,
+                                 current_task_payload_true, current_task_payload_false, current_task_pre_task,
                                  current_task_pre_task_payload, current_task_pre_task_duration))
             current_task_no += 1
-    
+
     except KeyError:
         logger.info(f'Task lore parsing complete. Read {current_task_no - 1} imp tasks.')
-        
+
     except:
         logger.critical('Could not parse imp task entries. Please make sure the appropriate structure is in place!')
         raise SystemExit(4)
-    
+
     loopRunner = True
-    
+
     try:
         while loopRunner:
             logger.info('The bell rings...')
-            
+
             logger.info('***********************************************************')
-            
+
             for imp in imp_tasks:
                 imp_logger_prefix = f'{imp.name} >>>'
-                
+
                 logger.info(f'{imp_logger_prefix} The imp has awakened.')
-                
+
                 logger.info(f'{imp_logger_prefix} The imp is stretching...')
                 try:
                     imp.stretch()
-                    sleep(imp.pre_task_duration)
+                    # pre-task sleep is deferred post imp work
                 except:
                     logger.exception(f'{imp_logger_prefix} The imp has encountered an error...')
                     # uncomment for debugging purposes only
                     #logger.error(traceback.format_exc())
-                
+
                 if imp.active:
+                    start_time = time()
+
                     logger.info(f'{imp_logger_prefix} The imp is working on his task...')
                     try:
                         # dynamically reload expected values when not in cron job mode
                         if not CRON_JOB_MODE:
                             configParser.read(CONF_FILE_PATH)
                             imp.expected = configParser[imp.header].get('expected')
-                        
+
                         imp.work()
-                        
+
                         logger.debug(f'{imp_logger_prefix} Imp output is: {imp.output}')
                         if imp.errors is not None:
                             logger.error(f'{imp_logger_prefix} The imp has encountered an ssh error: {imp.errors}')
-                        
+
                         imp.report()
-                        
+
                         logger.info(f'{imp_logger_prefix} [{imp.state}] is the outcome of the imp\'s task.')
-                    
+
                     except:
                         logger.exception(f'{imp_logger_prefix} The imp has encountered an error...')
                         # uncomment for debugging purposes only
                         #logger.error(traceback.format_exc())
-                    
+
+                    time_delta = round(time() - start_time, 2)
+                    logger.debug(f'{imp_logger_prefix} The imp has toiled for: {time_delta} seconds')
+                    # see how much time we have left to wait to match the specified pre_task_duration
+                    if time_delta < imp.pre_task_duration:
+                        remaining_pre_task_duration = round(imp.pre_task_duration - time_delta, 2)
+                        # the imp will work during pre-task then wait here a bit before it rests
+                        logger.debug(f'{imp_logger_prefix} The imp must wait for: {remaining_pre_task_duration} seconds.')
+                        sleep(remaining_pre_task_duration)
+                    else:
+                        logger.debug(f'{imp_logger_prefix} The imp has no need to wait.')
+
                     logger.info(f'{imp_logger_prefix} The imp has started resting...')
                     try:
                         imp.rest()
@@ -208,8 +221,10 @@ if __name__ == "__main__":
                         logger.exception(f'{imp_logger_prefix} The imp has encountered an error...')
                         # uncomment for debugging purposes only
                         #logger.error(traceback.format_exc())
-                
                 else:
+                    # we still need to wait for the pre-task duration even if the task is inactive
+                    sleep(imp.pre_task_duration)
+
                     logger.info(f'{imp_logger_prefix} The imp will remain idle and only pretend to work...')
                     try:
                         imp.idle()
@@ -217,19 +232,19 @@ if __name__ == "__main__":
                         logger.exception(f'{imp_logger_prefix} The imp has encountered an error...')
                         # uncomment for debugging purposes only
                         #logger.error(traceback.format_exc())
-                
+
                 logger.info(f'{imp_logger_prefix} The imp now sleeps.')
-            
+
             logger.info('***********************************************************')
-            
+
             if CRON_JOB_MODE:
                 logger.info('Cron job mode enabled. The imps will now be freed.')
                 loopRunner = False
             else:
                 logger.info('All imps are now asleep, waiting for the bell to ring.')
                 sleep(TASK_INTERVAL)
-    
+
     except SystemExit:
         logger.info('The imps scatter in chaos...')
-    
+
     logger.info('The imp bonds are shattered and they all flee.')
